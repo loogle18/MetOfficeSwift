24f6c20e13bc6113cee7ac37404e21dcbad833f4
loogle18 <sme@jetthoughts.com>
2017-06-22 04:45:36 +0300

    Added activity indicator.

--- MetOfficeSwift/Views/StationsTableViewController.swift
+++ MetOfficeSwift/Views/StationsTableViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class StationsTableViewController: UITableViewController {
     var stations = [Station]()
+    var loadingView: UIView!
+    var spinner: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        showActivityIndicator()
         StationDataService.getAllStationsData { stations in
             self.stations = stations.sorted { $0.name < $1.name }
             self.tableView.reloadData()
+            self.hideActivityIndicator()
         }
     }
 
@@ -41,6 +44,34 @@ class StationsTableViewController: UITableViewController {
             
             stationVC?.station = station
             stationVC?.climates = station.climates
+        }
+    }
+    
+    private func showActivityIndicator() {
+        DispatchQueue.main.async() {
+            self.loadingView = UIView()
+            self.loadingView.frame = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0)
+            self.loadingView.center = CGPoint(x: self.tableView.bounds.size.width / 2,
+                                              y: self.tableView.bounds.size.height / 2 - 44)
+            self.loadingView.backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 0.8)
+            self.loadingView.clipsToBounds = true
+            self.loadingView.layer.cornerRadius = 10
+            
+            self.spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+            self.spinner.frame = CGRect(x: 0.0, y: 0.0, width: 80.0, height: 80.0)
+            self.spinner.center = CGPoint(x: self.loadingView.bounds.size.width / 2,
+                                          y: self.loadingView.bounds.size.height / 2)
+            
+            self.loadingView.addSubview(self.spinner)
+            self.tableView.addSubview(self.loadingView)
+            self.spinner.startAnimating()
+        }
+    }
+    
+    private func hideActivityIndicator() {
+        DispatchQueue.main.async() {
+            self.spinner.stopAnimating()
+            self.loadingView.removeFromSuperview()
         }
     }
 }
